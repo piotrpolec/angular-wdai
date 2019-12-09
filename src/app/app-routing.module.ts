7a0badb746e2daa849851d32aa95f2cde3447d40
piotrpolec <piotr.jan.polec@gmail.com>
2019-12-09 23:45:07 +0100

    add routing files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddCourseComponent } from './admin-panel/add-course/add-course.component';
+import { AdminPanelComponent } from './admin-panel/admin-panel.component';
+import { EditCourseComponent } from './admin-panel/edit-course/edit-course.component';
+import { RemoveTeacherComponent } from './admin-panel/remove-teacher/remove-teacher.component';
+import { AddTeacherComponent } from './admin-panel/add-teacher/add-teacher.component';
+import { EditTeacherComponent } from './admin-panel/edit-teacher/edit-teacher.component';
+import { RemoveCourseComponent } from './admin-panel/remove-course/remove-course.component';
+import { CourseListComponent } from './course-list/course-list.component';
+import { TeacherListComponent } from './teacher-list/teacher-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "courses",component: CourseListComponent },
+  { path: "teachers", component: TeacherListComponent},
+  {
+  path: "admin-panel",
+  component: AdminPanelComponent,
+  children: [
+    { path: "add-course", component: AddCourseComponent },
+    { path: "edit-course/:id", component: EditCourseComponent},
+    { path: "remove-course/:id", component: RemoveCourseComponent },
+    { path: "add-teacher", component: AddTeacherComponent },
+    { path: "remove-teacher/:id", component: RemoveTeacherComponent },
+    { path: "edit-teacher/:id", component: EditTeacherComponent }
+  ]
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,14 @@ import { CourseDetailsComponent } from './course-list/course-details/course-deta
 import { TeacherListComponent } from './teacher-list/teacher-list.component';
 import { TeacherComponent } from './teacher-list/teacher/teacher.component';
 import { ModalModule } from 'ngx-bootstrap/modal';
+import { AddCourseComponent } from './admin-panel/add-course/add-course.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AdminPanelComponent } from './admin-panel/admin-panel.component';
+import { RemoveCourseComponent } from './admin-panel/remove-course/remove-course.component';
+import { EditCourseComponent } from './admin-panel/edit-course/edit-course.component';
+import { AddTeacherComponent } from './admin-panel/add-teacher/add-teacher.component';
+import { RemoveTeacherComponent } from './admin-panel/remove-teacher/remove-teacher.component';
+import { EditTeacherComponent } from './admin-panel/edit-teacher/edit-teacher.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +26,15 @@ import { ModalModule } from 'ngx-bootstrap/modal';
     CourseComponent,
     CourseDetailsComponent,
     TeacherListComponent,
-    TeacherComponent
+    TeacherComponent,
+    AddCourseComponent,
+    NavbarComponent,
+    AdminPanelComponent,
+    RemoveCourseComponent,
+    EditCourseComponent,
+    AddTeacherComponent,
+    RemoveTeacherComponent,
+    EditTeacherComponent
   ],
   imports: [
     BrowserModule,
